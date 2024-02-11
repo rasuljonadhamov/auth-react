584398225b92782178a543244032572440641a09
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-11 23:31:52 +0500

    Initial commit with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex">
+      <div className="left">
+        <img src="/public/logo.png" alt="Logo" />
+        <h1>Xush kelibsiz!</h1>
+        <p>Login parolingizni kiritib o‘z kabinetingizga kiring.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
