a5c43411a0120ea27e5de456a05a68b3181350a0
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-12 16:28:46 +0500

    finished styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import Input from "./components/Input";
 
 function App() {
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex flex-col items-center">
-        <img src="/public/logo.png" alt="Logo" className="mb-4" />
-        <h1 className="text-2xl mb-2 font-bold">Xush kelibsiz!</h1>
-        <p className="mb-4">
+    <div className="flex justify-between items-center p-[46px] ">
+      <div className="flex flex-col items-center justify-center">
+        <img
+          src="/public/logo.png"
+          alt="Logo"
+          className="mb-24 "
+          height={"45px"}
+          width={"125px"}
+        />
+        <h1 className="text-5xl mb-3 font-bold">Xush kelibsiz!</h1>
+        <p className="mb-11">
           Login parolingizni kiritib o‘z kabinetingizga kiring.
         </p>
 
@@ -23,15 +29,19 @@ function App() {
           />
           <button
             type="submit"
-            className="bg-blue-600 text-white px-4 py-2 rounded-md font-bold shadow hover:bg-blue-700"
+            className="bg-blue-600 w-full mt-2 text-white px-4 py-3 rounded-md font-bold shadow hover:bg-blue-700"
           >
             Kirish
           </button>
         </form>
+
+        <p className="mt-12 text-lg font-medium text-gray-500">
+          Copyright ©  2024 Vim kompaniyasi
+        </p>
       </div>
 
       <img
-        src="/public/male.png"
+        src="/male.png"
         alt="Male sitting photo"
         className="w-[820px] h-[660px] object-contain"
       />
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 const Input = ({ label, type, placeHolder }) => {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3 mb-5">
       <label className="block">{label}</label>
       <input
         className="block w-full px-4 py-2 rounded-md bg-gray-200 text-gray-700 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
